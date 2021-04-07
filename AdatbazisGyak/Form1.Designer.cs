ab9d51c1022aa4fe65db8cbfc9ebf27e047e6a6b
dregelyimi <78902226+dregelyimi@users.noreply.github.com>
2021-04-07 15:06:20 +0200

    Órai munka (dataGridView)

---
+++ AdatbazisGyak/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace AdatbazisGyak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.btnTorol = new System.Windows.Forms.Button();
+            this.txtTorol = new System.Windows.Forms.TextBox();
+            this.btnReset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(30, 69);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(255, 116);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(27, 24);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(41, 13);
+            this.Label1.TabIndex = 1;
+            this.Label1.Text = "Adatok";
+            // 
+            // btnTorol
+            // 
+            this.btnTorol.Location = new System.Drawing.Point(30, 229);
+            this.btnTorol.Name = "btnTorol";
+            this.btnTorol.Size = new System.Drawing.Size(75, 23);
+            this.btnTorol.TabIndex = 2;
+            this.btnTorol.Text = "Törlés";
+            this.btnTorol.UseVisualStyleBackColor = true;
+            this.btnTorol.Click += new System.EventHandler(this.btnTorol_Click);
+            // 
+            // txtTorol
+            // 
+            this.txtTorol.Location = new System.Drawing.Point(30, 203);
+            this.txtTorol.Name = "txtTorol";
+            this.txtTorol.Size = new System.Drawing.Size(100, 20);
+            this.txtTorol.TabIndex = 3;
+            // 
+            // btnReset
+            // 
+            this.btnReset.Location = new System.Drawing.Point(30, 40);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(75, 23);
+            this.btnReset.TabIndex = 4;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.txtTorol);
+            this.Controls.Add(this.btnTorol);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Button btnTorol;
+        private System.Windows.Forms.TextBox txtTorol;
+        private System.Windows.Forms.Button btnReset;
+    }
+}
+
---
+++ AdatbazisGyak/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdatbazisGyak
+{
+    public partial class Form1 : Form
+    {
+        List<Auto> autokLista = new List<Auto>();
+        DataTable raktarTable = new DataTable();
+        public Form1()
+        {
+            InitializeComponent();
+
+            autokLista.Add(new Auto("Pál", "BMW", "zöld"));
+            autokLista.Add(new Auto("Petra", "VW", "piros"));
+            autokLista.Add(new Auto("Melinda", "Ford", "fekete"));
+            autokLista.Add(new Auto("Péter", "BMW", "ezüst"));
+
+            TablazatsemaLetrehoz();
+            TablazatFeltolt();
+
+            dataGridView1.DataSource = raktarTable;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TablazatsemaLetrehoz() 
+        {
+            DataColumn gyartoColumn = new DataColumn("Gyártó", typeof(string));
+            DataColumn szinColumn = new DataColumn("Szín", typeof(string));
+            DataColumn nevColumn = new DataColumn("Név", typeof(string));
+
+            DataColumn autoIdColumn = new DataColumn("autoId", typeof(int));
+            autoIdColumn.Caption = "Autó ID";
+            autoIdColumn.ReadOnly = true;
+            autoIdColumn.AllowDBNull = false;
+            autoIdColumn.Unique = true;
+            autoIdColumn.AutoIncrement = true;
+            autoIdColumn.AutoIncrementSeed = 0;
+            autoIdColumn.AutoIncrementStep = 1;
+
+
+            raktarTable.Columns.AddRange(new DataColumn[] {autoIdColumn, gyartoColumn, szinColumn, nevColumn });
+        }
+
+        private void TablazatFeltolt() 
+        {
+            foreach (Auto auto in autokLista)
+            {
+                DataRow dr = raktarTable.NewRow();
+
+                dr["Gyártó"] = auto.Gyarto;
+                dr["Szín"] = auto.Szin;
+                dr["Név"] = auto.Nev;
+
+                raktarTable.Rows.Add(dr);
+            }
+        }
+
+        private void btnTorol_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                raktarTable.Rows[int.Parse(txtTorol.Text)].Delete();
+                raktarTable.AcceptChanges();
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+            raktarTable.Rows.Clear();
+
+            TablazatFeltolt();
+        }
+    }
+}
